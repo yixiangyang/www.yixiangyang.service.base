18b54db03189bf0c62df1eaace6ac884203a2149
xiangyang <15138778469@163.com>
2017-12-25 18:08:25 +0800

    update

---
+++ src/test/java/www/yixiangyang/servicebase/www/yixiangyang/servicebase/ApplicationTests.java
@@ -0,0 +1,16 @@
+package www.yixiangyang.servicebase.www.yixiangyang.servicebase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
